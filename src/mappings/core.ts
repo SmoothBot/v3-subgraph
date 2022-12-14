47e1752cd351e8bf6afca5c2a8cd82227ad8b3c7
SmoothBot <smoothbot123@gmail.com>
2022-12-14 11:51:41 +1100

    Adding pool minute data with fees

--- src/mappings/core.ts
+++ src/mappings/core.ts
@@ -15,6 +15,7 @@ import { findEthPerToken, getEthPriceInUSD, getTrackedAmountUSD, sqrtPriceX96ToT
 import {
   updatePoolDayData,
   updatePoolHourData,
+  updatePoolMinuteData,
   updateTickDayData,
   updateTokenDayData,
   updateTokenHourData,
@@ -30,6 +31,7 @@ export function handleInitialize(event: Initialize): void {
 
   updatePoolDayData(event)
   updatePoolHourData(event)
+  updatePoolMinuteData(event)
 }
 
 export function handleMint(event: MintEvent): void {
@@ -135,6 +137,7 @@ export function handleMint(event: MintEvent): void {
   updateUniswapDayData(event)
   updatePoolDayData(event)
   updatePoolHourData(event)
+  updatePoolMinuteData(event)
   updateTokenDayData(token0 as Token, event)
   updateTokenDayData(token1 as Token, event)
   updateTokenHourData(token0 as Token, event)
@@ -237,6 +240,7 @@ export function handleBurn(event: BurnEvent): void {
   updateUniswapDayData(event)
   updatePoolDayData(event)
   updatePoolHourData(event)
+  updatePoolMinuteData(event)
   updateTokenDayData(token0 as Token, event)
   updateTokenDayData(token1 as Token, event)
   updateTokenHourData(token0 as Token, event)
@@ -393,6 +397,7 @@ export function handleSwap(event: SwapEvent): void {
   let uniswapDayData = updateUniswapDayData(event)
   let poolDayData = updatePoolDayData(event)
   let poolHourData = updatePoolHourData(event)
+  let poolMinuteData = updatePoolMinuteData(event)
   let token0DayData = updateTokenDayData(token0 as Token, event)
   let token1DayData = updateTokenDayData(token1 as Token, event)
   let token0HourData = updateTokenHourData(token0 as Token, event)
@@ -412,6 +417,11 @@ export function handleSwap(event: SwapEvent): void {
   poolHourData.volumeToken0 = poolHourData.volumeToken0.plus(amount0Abs)
   poolHourData.volumeToken1 = poolHourData.volumeToken1.plus(amount1Abs)
   poolHourData.feesUSD = poolHourData.feesUSD.plus(feesUSD)
+
+  poolMinuteData.volumeUSD = poolMinuteData.volumeUSD.plus(amountTotalUSDTracked)
+  poolMinuteData.volumeToken0 = poolMinuteData.volumeToken0.plus(amount0Abs)
+  poolMinuteData.volumeToken1 = poolMinuteData.volumeToken1.plus(amount1Abs)
+  poolMinuteData.feesUSD = poolMinuteData.feesUSD.plus(feesUSD)
 
   token0DayData.volume = token0DayData.volume.plus(amount0Abs)
   token0DayData.volumeUSD = token0DayData.volumeUSD.plus(amountTotalUSDTracked)
